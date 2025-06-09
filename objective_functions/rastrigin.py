6e94f73b73aa175196b3ecb173ec41513f5c5a5c
MarioSomodi <mario0.0somodi@gmail.com>
2025-06-09 22:12:08 +0200

    fix import, and follow python naming rules

--- objective_functions/rastrigin.py
+++ objective_functions/rastrigin.py
@@ -1,9 +1,10 @@
 import numpy as np
-from .base_objective import BaseObjective
+from objective_functions.base_objective import BaseObjective
+
 
 class RastriginObjective(BaseObjective):
-    def __init__(self, dim: int, A: float = 10):
-        self.A = A
+    def __init__(self, dim: int, a: float = 10):
+        self.a = a
         self.dim = dim
         super().__init__(name="Rastrigin", bounds=(-5.12, 5.12), global_min=np.zeros(dim))
 
@@ -12,4 +13,4 @@ class RastriginObjective(BaseObjective):
         Rastrigin function: f(x) = An + Σ[x_i² - A * cos(2πx_i)]
         Global minimum at x = 0^n, value = 0
         """
-        return self.A * self.dim + sum(x_i ** 2 - self.A * np.cos(2 * np.pi * x_i) for x_i in x)
+        return self.a * self.dim + sum(x_i ** 2 - self.a * np.cos(2 * np.pi * x_i) for x_i in x)
